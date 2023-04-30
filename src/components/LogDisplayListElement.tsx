c5b2eab34851dc205e9abd31b9e8d61961f4f91a
techchrism <chris@techchrism.me>
2023-04-30 14:28:58 -0700

    add collapsable json view and sidebar search functionality

--- src/components/LogDisplayListElement.tsx
+++ src/components/LogDisplayListElement.tsx
@@ -11,6 +11,23 @@ const LogDisplayListElement: Component<LogDisplayListElementProps> = (props) => 
     const time = createMemo(() => new Date(first().time))
     const length = createMemo(() => props.item.buffer.reduce((prev, curr) => prev + curr.data.length, 0))
 
+    const title = createMemo(() => {
+        if(props.item.data.hasOwnProperty('presence') && Object.keys(props.item.data).length === 1) {
+            return 'Presence Update'
+        }
+        return 'Item'
+    })
+
+    const subtitle = createMemo(() => {
+        if(props.item.data.hasOwnProperty('iq') && props.item.data.iq.hasOwnProperty('id')) {
+            return props.item.data.iq.id
+        } else if(props.item.data.hasOwnProperty('presence') && Object.keys(props.item.data).length === 1) {
+            try {
+                const presenceData = JSON.parse(atob(props.item.data.presence.games.valorant.p))
+            } catch(ignored) {}
+        }
+    })
+
     return (
         <>
             <div class="flex flex-row items-center space-x-3 w-full">
@@ -20,9 +37,9 @@ const LogDisplayListElement: Component<LogDisplayListElementProps> = (props) => 
                     </Show>
                 </div>
                 <div class="flex-grow">
-                    Item
-                    <Show when={props.item.data.hasOwnProperty('iq') && props.item.data.iq.hasOwnProperty('id')}>
-                        <p>{props.item.data.iq.id}</p>
+                    {title()}
+                    <Show when={subtitle() !== undefined}>
+                        <p>{subtitle()}</p>
                     </Show>
 
                 </div>
--- src/pages/LogDisplay.tsx
+++ src/pages/LogDisplay.tsx
@@ -1,7 +1,17 @@
-import {Component, createMemo, createSignal, For} from 'solid-js'
+import {Component, ComponentProps, createEffect, createMemo, createSignal, For, onMount, Show} from 'solid-js'
 import {ParsedLog} from '../fileParser'
 import {FiFilter} from 'solid-icons/fi'
 import LogDisplayListElement from '../components/LogDisplayListElement'
+import '@alenaksu/json-viewer'
+
+// Types for the json-viewer component, modified from https://stackoverflow.com/a/72239265
+declare module 'solid-js' {
+    namespace JSX {
+        interface IntrinsicElements {
+            "json-viewer": ComponentProps<"div"> & { data: any }
+        }
+    }
+}
 
 export type LogDisplayProps = {
     parsedLog: ParsedLog
@@ -9,22 +19,35 @@ export type LogDisplayProps = {
 
 const LogDisplay: Component<LogDisplayProps> = (props) => {
     const [activeIndex, setActiveIndex] = createSignal(0)
-    const activeItem = createMemo(() => props.parsedLog.xml[activeIndex()])
+    const [search, setSearch] = createSignal('')
+
+    const items = createMemo(() => {
+        return props.parsedLog.xml
+            .filter(item => item.buffer.some(buffer => buffer.data.includes(search())))
+    })
+    const activeItem = createMemo(() => items()[activeIndex()])
+
+    let jsonViewer
+    createEffect(() => {
+        if(activeItem()) {
+            jsonViewer.expandAll()
+        }
+    })
 
     return (
         <>
             <div class="min-h-screen md:grid md:grid-cols-main">
-                <aside class="md:sticky top-0 left-0 md:h-screen overflow-y-auto bg-gray-50 dark:bg-gray-800" aria-label="Sidebar">
+                <aside class="md:sticky top-0 left-0 md:h-screen overflow-y-auto" aria-label="Sidebar">
                     <div class="sticky top-0 z-10 bg-base-300 py-2">
                         <h1 class="text-xl font-semibold text-center mb-2">Log Display</h1>
 
                         <div class="mx-2 flex flex-row">
-                            <input type="text" class="input mr-2 flex-grow" placeholder="Search..."/>
+                            <input type="text" class="input mr-2 flex-grow" placeholder="Search..." oninput={(e) => {setSearch((e.target as HTMLInputElement).value)}}/>
                             <button class="btn btn-square"><FiFilter title="Filter"/></button>
                         </div>
                     </div>
                     <ul class="menu bg-base-200 text-base-content">
-                        <For each={props.parsedLog.xml}>
+                        <For each={items()}>
                             {(item, index) => (<>
                                 <li>
                                     <a class="w-full py-1 px-2 border-y border-base-100" classList={{active: index() === activeIndex()}} onClick={() => setActiveIndex(index())}>
@@ -36,9 +59,11 @@ const LogDisplay: Component<LogDisplayProps> = (props) => {
                     </ul>
                 </aside>
                 <main class="p-5">
-                    <code class="whitespace-pre-wrap break-all">
-                        {JSON.stringify(activeItem().data, null, 4)}
-                    </code>
+                    <Show when={activeItem()}>
+                        <code class="whitespace-pre-wrap break-all">
+                            <json-viewer data={activeItem().data} ref={jsonViewer}></json-viewer>
+                        </code>
+                    </Show>
                 </main>
             </div>
         </>
